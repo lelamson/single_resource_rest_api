5171398887a6e77d5686f946ad53002fa85835e0
Lamson Le <ll217@u.washington.edu>
2015-06-02 11:37:17 -0700

    add create delete test

--- test/karma_tests/cards_controller_test.js
+++ test/karma_tests/cards_controller_test.js
@@ -46,6 +46,42 @@ describe('cards controller', function () {
       expect($scope.cards[0].spell).toBe('Fireball');
       expect($scope.cards[0].color).toBe('Red');
       expect($scope.cards[0].cost).toBe(2);
+      expect($scope.errors.length).toBe(0);
+    });
+
+    it('should create a new card', function () {
+      $scope.newCard = {spell: 'Angel', color: 'White', cost: 4};
+      $httpBackend.expectPOST('/magic/cards').respond(200, {_id: 102, spell: 'Angel', color: 'White', cost: 4});
+      $scope.createCard();
+      $httpBackend.flush();
+      expect($scope.cards[0]._id).toBe(102);
+      expect($scope.cards[0].spell).toBe('Angel');
+      expect($scope.cards[0].color).toBe('White');
+      expect($scope.cards[0].cost).toBe(4);
+      expect($scope.newCard).toBe(null);
+      expect($scope.errors.length).toBe(0);
+    });
+
+    it('should delete a card', function () {
+      var card = {_id: 103, spell: 'Raven', color: 'Black', cost: 1};
+      $scope.cards.push(card);
+      $httpBackend.expectDELETE('/magic/cards/103').respond(200, {msg: 'successful removal'});
+      expect($scope.cards.indexOf(card)).not.toBe(-1);
+      $scope.removeCard(card);
+      expect($scope.cards.indexOf(card)).toBe(-1);
+      $httpBackend.flush();
+      expect($scope.errors.length).toBe(0);
+    });
+
+    it('should delete a card even with server error', function () {
+      var card = {_id: 104, spell: 'Fall', color: 'Blue', cost: 1};
+      $scope.cards.push(card);
+      $httpBackend.expectDELETE('/magic/cards/104').respond(500, {msg: 'card delete error'});
+      expect($scope.cards.indexOf(card)).not.toBe(-1);
+      $scope.removeCard(card);
+      expect($scope.cards.indexOf(card)).toBe(-1);
+      $httpBackend.flush();
+      expect($scope.errors.length).toBe(1);
     });
 
   });
