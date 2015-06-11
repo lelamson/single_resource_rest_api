45cb4de2798f7da0f4c8505e00cec00c9d747155
Lamson Le <ll217@u.washington.edu>
2015-06-11 10:07:37 -0700

    fix auth decode id, karma test, server test

--- test/server/cards_api_test.js
+++ test/server/cards_api_test.js
@@ -31,14 +31,14 @@ describe('cards REST API', function() {
       this.testUser = data;
     }.bind(this));
 
-    var testCard = new Card({spell: 'Growth', color: 'Green', cost: 1});
+    var testCard = new Card({authorId: '101', spell: 'Growth', color: 'Green', cost: 1});
     testCard.save(function (err, data) {
       if (err) throw err;
 
       this.testCard = data;
     }.bind(this));
 
-    var testCard2 = new Card({spell: 'Acid Rain', color: 'Black', cost: 4});
+    var testCard2 = new Card({authorId: '102', spell: 'Acid Rain', color: 'Black', cost: 4});
     testCard2.save(function (err, data) {
       if (err) throw err;
 
@@ -67,7 +67,7 @@ describe('cards REST API', function() {
   it('should be able to create new Card', function (done) {
     chai.request('localhost:7000')
     .post('/magic/cards')
-    .send({spell: 'Fireball', color: 'Red', cost: 2, eat: this.testUser.basic.eatTest})
+    .send({authorId: '201', spell: 'Fireball', color: 'Red', cost: 2, eat: this.testUser.basic.eatTest})
     .end(function (err, res) {
       expect(err).to.eql(null);
       expect(res.body.spell).to.eql('Fireball');
